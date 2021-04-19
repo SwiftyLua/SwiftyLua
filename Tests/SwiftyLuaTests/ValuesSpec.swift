d322533c33faec211761aa160a838310d4c5009c
Thomas Bonk <thomas@meandmymac.de>
2021-04-19 18:19:05 +0200

    created test cases for the Value enum

--- Tests/SwiftyLuaTests/ValuesSpec.swift
+++ Tests/SwiftyLuaTests/ValuesSpec.swift
@@ -29,14 +29,98 @@ final class ValuesSpec: QuickSpec {
   override func spec() {
 
     describe("Test whether returned values are valid") {
-      /*var stringVal: Value { StringValue("stringVal", name: "stringVal") }
+      it("Value.bool is valid") {
+        let vm = LuaVirtualMachine()
 
-      it("StringValue is valid") {
-        let val: String = stringVal.value()
+        Value.bool(value: true, name: "boolVal").push(vm)
 
-        expect(val).to(equal("stringVal"))
-        //expect(stringVal.value()).to(be("stringVal"))
-      }*/
+        let val: Bool = try Value.bool().pop(vm).value()
+
+        expect(val).to(equal(true))
+      }
+
+      it("Value.double is valid") {
+        let vm = LuaVirtualMachine()
+
+        Value.double(value: 13.37, name: "doubleVal").push(vm)
+
+        let val: Double = try Value.double().pop(vm).value()
+
+        expect(val).to(equal(13.37))
+      }
+
+      it("Value.int is valid") {
+        let vm = LuaVirtualMachine()
+
+        Value.int(value: 1337, name: "intVal").push(vm)
+
+        let val: Int64 = try Value.int().pop(vm).value()
+
+        expect(val).to(equal(1337))
+      }
+
+      it("Value.string is valid") {
+        let vm = LuaVirtualMachine()
+
+        Value.string(value: "aString", name: "strVal").push(vm)
+
+        let val: String = try Value.string().pop(vm).value()
+
+        expect(val).to(equal("aString"))
+      }
+
+      it("Value.void is valid") {
+        let vm = LuaVirtualMachine()
+
+        Value.void(value: (), name: "voidVal").push(vm)
+
+        let void = try Value.void().pop(vm)
+        let val: Void = try void.value()
+
+        expect(val == ()).to(beTruthy())
+      }
+    }
+
+    describe("Test whether type mismatch errors are thrown") {
+      it("Value.bool mismatches Value.string") {
+        let vm = LuaVirtualMachine()
+
+        Value.bool(value: true, name: "val").push(vm)
+
+        expect {
+          let _: String = try Value.string().pop(vm).value()
+        }.to(throwError())
+      }
+
+      it("Value.bool mismatches Value.double") {
+        let vm = LuaVirtualMachine()
+
+        Value.bool(value: true, name: "val").push(vm)
+
+        expect {
+          let _: Double = try Value.double().pop(vm).value()
+        }.to(throwError())
+      }
+
+      it("Value.bool mismatches Value.int") {
+        let vm = LuaVirtualMachine()
+
+        Value.bool(value: true, name: "val").push(vm)
+
+        expect {
+          let _: Int64 = try Value.int().pop(vm).value()
+        }.to(throwError())
+      }
+
+      it("Value.string mismatches Value.bool") {
+        let vm = LuaVirtualMachine()
+
+        Value.string(value: "aString", name: "val").push(vm)
+
+        expect {
+          let _: Bool = try Value.bool().pop(vm).value()
+        }.to(throwError())
+      }
     }
 
   }
